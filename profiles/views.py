38dce91e5ec116a60d575ebffeaa6a39331d6bd1
arasbulba <araz.gasimov@gmail.com>
2021-07-29 20:32:11 +0400

    profile update

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth import models
-from django.forms import widgets
-from profiles.forms import EditProfileForm, EditUserForm
-import profiles
+from django.http import request
+from django.urls.base import reverse_lazy
+from profiles.forms import EditProfileForm
 from profiles.models import Profile
 from django.contrib.auth.models import User
-from django.views.generic import DetailView, UpdateView, FormView
+from django.views.generic import DetailView, UpdateView
 from cimbomwebblog.models import Post
-from django.shortcuts import render 
+from django.shortcuts import render
+from django.urls import reverse
 
 
 class ProfileDetailView(DetailView):
@@ -22,15 +22,27 @@ class ProfileDetailView(DetailView):
         user = self.get_object()
         context = super().get_context_data(**kwargs)
         context['total_posts'] = Post.objects.filter(author=user).count()
+        context['all_posts'] = Post.objects.filter(author=user)
         return context
 
 
 class ProfileEditView(UpdateView):
     template_name = "account/edit.html"
     model = Profile
-    success_url = "/"
     form_class = EditProfileForm
+    #success_url = reverse_lazy('profiles:details')
+    #success_url = "/"
+    
+  
 
     def get_object(self):
         return self.request.user.profile
 
+    def user_author(self):
+        if not request.user.profile.is_authenticated:
+            return render(request, 'index.html')
+    
+    def get_success_url(self):
+        return reverse('profiles:details', args=[self.request.user.username])
+
+ 
